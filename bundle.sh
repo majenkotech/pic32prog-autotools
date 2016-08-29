61015e19a946a4d35433fa5d3a9a9d0896fe57e6
Matt Jenkins <matt@majenko.co.uk>
2016-08-29 10:51:36 +0100

    Added script to make release bundles

---
+++ bundle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VER=`bin/pic32prog-debian-64 2>/dev/null | grep Version | awk '{print $7}'`
+
+mkdir -p release
+
+function bundle {
+    SRC=$1
+    OS=$2
+    mkdir -p tmp/pic32prog
+    cp $SRC tmp/pic32prog/pic32prog
+    tar -C tmp -zcf release/pic32prog-${OS}-${VER}.tar.gz pic32prog
+    rm -rf tmp
+    (
+        cd release
+        sha256sum pic32prog-armlinux-2.1.24.tar.gz | awk '{print "\nAlgorithm       Hash                                                                   Path\n---------       ----                                                                   ----\nSHA256          " toupper($1) "       " $2 "\n\n"}' > pic32prog-${OS}-${VER}.asc
+    )
+}
+
+bundle bin/pic32prog-debian-64 linux64
+bundle bin/pic32prog-debian-32 linux32
+bundle bin/pic32prog-debian-armhf armlinux
+bundle bin/pic32prog-win32.exe windows
+bundle bin/pic32prog-osx-fat macosx
